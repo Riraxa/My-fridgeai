6d8c9bb6206940824d51871e488b63d8115432a5
My-frigedai <auos.05.04-conan@au.com>
2026-01-28 21:31:58 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,6 +21,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Fridge AI",
   description: "AIで食材管理・献立作成ができるアプリ",
+  verification: {
+    google: "MC63axKn7PuRNgUa2LEQ-Ua70QSvtiCVRW9RgloHW0g",
+  },
   // ページ遷移のパフォーマンス最適化
   other: {
     "theme-color": "#ff914d",
